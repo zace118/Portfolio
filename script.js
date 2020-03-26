d84667956559e91282d35845a021095d19fb9075
Zac Eskridge <zace118@gmail.com>
2020-03-25 22:06:33 -0700

    Changed up what's on my portfolio section to include the newest group project. Also added in a timer for the index page that will auto-redirect the user if they don't know to click on my name.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-$('#myName').click(function () {
-    // wipe page and start about me page
-    window.location.href = "./about.html";
-    return false;
-})
+$(document).ready(function () {
 
+    // For some reason I thought everyone would understand to just click on my name. I don't think that's the case....but I do still like the pure amount of simplicity on it. 
+    $('#myName').click(function () {
+        // wipe page and redirect to about me page
+        window.location.href = "./about.html";
+        return false;
+    })
 
+    // This function waits 8 seconds and then will automatically redirect the user if they don't realize they have to click on my name.
+    setTimeout(function () {
+        window.location.href = './about.html';
+    }, 8000);
+
+});
